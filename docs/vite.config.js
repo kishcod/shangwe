af7204a94c6cac77a3e9d9c46dc9b77e3ecb8a89
kishcod <147909829+kishcod@users.noreply.github.com>
2025-11-14 17:16:21 +0300

    Delete docs directory

--- docs/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Update base to your repo name if different
-export default defineConfig({
-  base: '/shangwe-frontend/',
-  plugins: [react()],
-})
